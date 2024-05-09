931014c6dea553ee1f3b8c7f267f6af9de8d4f26
Daniel Shin <140947149+danielkshin@users.noreply.github.com>
2024-05-08 22:16:13 -0700

    Add project files

---
+++ README.md
@@ -0,0 +1,5 @@
+# VOTD Lock Screen Widget
+**A Scriptable lock screen widget that displays the YouVersion Bible verse of the day.**  
+I just wanted a clean lock screen widget of the verse of the day. I found out it didn't exist, so instead of searching for an alternative, I spent several hours creating this.  
+
+<img src="preview.png" width="400px"/>
---
+++ widget.js
@@ -0,0 +1,48 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-purple; icon-glyph: bible;
+
+/**
+ * YouVersion Bible Verse of the Day Lock Screen Widget
+ * A mouthful, yes.
+ * Seriously. Why isn't this already a thing?
+ * And why did I have to spend two hours making this?
+ */
+
+// Customizations
+const textSize = 10;
+const textOpacity = 0.7;
+const bibleVersion = 111; // NIV
+
+// Create widget
+let widget = new ListWidget();
+// Redirect to Bible app when clicked
+widget.url = 'youversion://';
+
+// Find day of the year (0-365)
+const date = new Date();
+const dayOfYear = Math.floor((date - new Date(date.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24) - 1;
+
+// Get all Verse of the Days
+let request = new Request('https://nodejs.bible.com/api/moments/votd/3.1');
+let votds = await request.loadJSON();
+votds = votds['votd'];
+
+// Get Verse of the Day for today
+request = new Request(`https://nodejs.bible.com/api/bible/verse/3.1?id=${bibleVersion}&reference=${votds[dayOfYear]['usfm'][0]}`);
+let votd = await request.loadJSON();
+
+// Add the verse content
+let content = widget.addText(votd['content']);
+content.font = Font.lightMonospacedSystemFont(textSize);
+content.textOpacity = textOpacity;
+content.minimumScaleFactor = 6 / textSize;
+
+// Add the verse reference
+let reference = widget.addText(votd['reference']['human']);
+reference.font = Font.regularMonospacedSystemFont(textSize);
+reference.textOpacity = textOpacity;
+
+// Set widget
+Script.setWidget(widget);
+Script.complete();
